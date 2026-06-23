d8fc95eeb281da1e39adb9aff5ae1cdc857bb69a
P-kalala27 <jpmulyanga@gmail.com>
2026-06-23 12:46:50 +0200

    SPRINT_2 : debut du sprint 2 et structure du sprint

--- .next/types/validator.ts
+++ .next/types/validator.ts
@@ -56,6 +56,15 @@ type RouteHandlerConfig<Route extends AppRouteHandlerRoutes = AppRouteHandlerRou
   type __Unused = __Check
 }
 
+// Validate ../../src/app/(app)/profile/page.tsx
+{
+  type __IsExpected<Specific extends AppPageConfig<"/profile">> = Specific
+  const handler = {} as typeof import("../../src/app/(app)/profile/page.js")
+  type __Check = __IsExpected<typeof handler>
+  // @ts-ignore
+  type __Unused = __Check
+}
+
 // Validate ../../src/app/(auth)/auth/page.tsx
 {
   type __IsExpected<Specific extends AppPageConfig<"/auth">> = Specific
